c829944123d73c8eb625c41feccf93ff19f205c1
Sagar Goel <sagar.goel1911@gmail.com>
2024-01-09 18:20:31 +0530

    Add static keyword demo

---
+++ static.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Human{
+    public:
+    static int population;//declare a static variable
+    int age=0;
+    static void populationSquared(){
+        // cout<<population*age<<endl;//error->static functions cannot use non static members
+        cout<<population*population<<endl;
+        // cout<<this->population;//error-> this cannot be used in a static member function
+    }
+    void showPop(){
+        cout<<this->population<<endl;//this is valid, but not recommended, as a static member belongs to the class, not an object
+        population++;
+    }
+
+};
+int Human::population=5;//initialise a static variable
+int main(){
+    Human Sagar;
+    Sagar.showPop();
+    cout<<Human::population<<endl;//use a static variable
+    Human::populationSquared();
+    //a static member/function can be used by an object too but not advisable as a static member belongs to a class, not a specific object
+    return 0;
+}

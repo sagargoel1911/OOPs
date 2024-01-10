380bfc1acd6ea5f7378c73a070267a4e18483692
Sagar Goel <sagar.goel1911@gmail.com>
2024-01-10 19:33:43 +0530

    Add virtual destructor tutorial

---
+++ Virtual_Destructor.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Base1{
+    public:
+    Base1(){
+        cout<<"Base 1 constructor"<<endl;
+    }
+    ~Base1(){
+        cout<<"Base 1 destructor"<<endl;
+    }
+};
+class Base2{
+    public:
+    Base2(){
+        cout<<"Base 2 constructor"<<endl;
+    }
+    virtual ~Base2(){
+        cout<<"Base 2 destructor"<<endl;
+    }
+};
+class Inherited1:  public Base1{
+    public:
+    Inherited1(){
+        cout<<"Inherited 1 constructor"<<endl;
+    }
+    ~Inherited1(){
+        cout<<"Inherited 1 destructor"<<endl;
+    }
+};
+class Inherited2:  public Base2{
+    public:
+    Inherited2(){
+        cout<<"Inherited 2 constructor"<<endl;
+    }
+    ~Inherited2(){
+        cout<<"Inherited 2 destructor"<<endl;
+    }
+};
+int main(){
+    Base1*b1 = new Inherited1();
+    delete b1;// only Base1 destructor
+    Base2*b2 = new Inherited2();
+    delete b2;// both Inherited2 and Base2 destructed
+    return 0;
+}

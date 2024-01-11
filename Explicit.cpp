2a6bf8963504ce090209d9478cfdba8b042330e0
Sagar Goel <sagar.goel1911@gmail.com>
2024-01-11 19:25:13 +0530

    Add Explicit tutorial

---
+++ Explicit.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class A{
+    public:
+    int num;
+    A(int num){
+        this->num=num;
+    }
+};
+class B{
+    public:
+    int num;
+    explicit B(int num){
+        this->num=num;
+    }
+};
+int main(){
+    A a=10;
+    cout<<a.num<<endl;
+    // B b=10; // error as this is what explicit does
+    B b(10);
+    cout<<b.num<<endl;
+    return 0;
+}
